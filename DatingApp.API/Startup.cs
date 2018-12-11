cf6742a4221af2e1dea56c55b5a55edd5fd10740
Daniel <daniel@cardexchangesolutions.com>
2018-12-11 10:01:56 +0000

    End of Section 10

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -82,7 +82,7 @@ namespace DatingApp.API
 
             // app.UseHttpsRedirection();
             //seeder.SeedUsers();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
         }
